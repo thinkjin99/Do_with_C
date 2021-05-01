9ec0df3d3b40f381d128fa1089c0b797ce517e2b
Jin <mjin1789@gmail.com>
2021-05-01 13:13:08 +0900

    fibo final

--- 0417/fibonaci.cpp
+++ 0417/fibonaci.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int fiboNums[3] = {0,1,0};
-    int num;
-    cin >> num;
-    for (int i = 0; i < num; i++){
-        fiboNums[2] = fiboNums[1];
-        fiboNums[1] = fiboNums[0];
-        fiboNums[0] = fiboNums[1] + fiboNums[2];
+int Fibo(int n){
+    int fiboTerms[2] = {1,1};
+    int result = 1;
+    for (int i = 2; i < n; i++){
+        result = fiboTerms[0] + fiboTerms[1];
+        fiboTerms[1] = fiboTerms[0];
+        fiboTerms[0] = result;
     }
-    cout << fiboNums[0] << endl;
+   return result;
 }
+   
