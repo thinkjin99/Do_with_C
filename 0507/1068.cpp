295de067cb887c6869d0b1ed69de16868a9fa766
Jin <thinkjin99@gmail.com>
2021-05-08 22:44:24 +0900

    BOJ 1068 tree by dfs

--- 0507/1068.cpp
+++ 0507/1068.cpp
@@ -1,38 +1,47 @@
 #include<iostream>
 #include<vector>
-#include<queue>
 using namespace std;
-class Node{
-    int data;
-    vector<Node> node;
-    Node* parent;
-    public:
-        Node(Node* parent, int data):parent(parent),data(data){}
-};
 
-class Tree{
-    public:
-        Node* head;
-        int countLeaf();
-        void pushNode(vector<int>& v);
-};
+void removeNode(int num, vector< vector<int> > &childs, bool(&visited)[50]){
+    while(childs[num].size() != 0){
+        int back = childs[num].back();
+        childs[num].pop_back();
+        removeNode(back,childs,visited);
+    }
+    visited[num] = false;
+    return;
+}
 
 int main(){
     int nodeNum;
     cin >> nodeNum;
     vector< vector<int> > childs (50,vector<int>(0,0));
-    Tree* t = new Tree();
-    int head; cin >> head;
-    t->head = new Node(nullptr,head);
-    for(int i = 1; i < nodeNum; i++){
-        int num; cin >> num;
-        childs[num].push_back(i);
+
+    bool vistied[50] = {false};
+    int parentIndex[50];
+    for(int i = 0; i < nodeNum; i++){
+        int parent; cin >> parent; // parent를 입력받고 
+        if(parent != -1){
+            parentIndex[i] = parent;
+            childs[parent].push_back(i); // 자식 벡터의 각각의 parent의 위치에 n번째 노드를 추가
+        }
+        vistied[i] = true; // i번째를 추가하면 i 노드는 존재하는 것 이므로
     }
 
-    for(int i = 0; i != childs.size(); i++){
-        for(int j = 0; j != childs[i].size(); j++){
-            cout << childs[i][j] << "  ";
-        }
-        cout << endl;
+    int removeIndex;
+    cin >> removeIndex;
+    removeNode(removeIndex, childs, vistied);
+    for (int i = 0; i < childs[parentIndex[removeIndex]].size(); i++){
+        if(childs[parentIndex[removeIndex]][i] == removeIndex)
+            childs[parentIndex[removeIndex]].erase(childs[parentIndex[removeIndex]].begin() + i);
     }
+    
+    int leaf = 0;
+    for(int i = 0; i < 50; i++){
+        if(vistied[i]){
+            if(childs[i].size() == 0)
+                leaf++;
+        }
+    }
+    cout << leaf << endl;
 }
