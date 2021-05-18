815d6a17950ec669df950921b335217011ecada5
Jin <thinkjin99@gmail.com>
2021-05-18 12:24:23 +0900

    BFS DFS complete

--- 0515/1260.cpp
+++ 0515/1260.cpp
@@ -1,19 +1,35 @@
 #include<vector>
 #include<algorithm>
 #include<iostream>
+#include<queue>
 using namespace std;
 vector<int> vertaxArray[1001];
 bool visit[1001] = {false};
 void dfs(int vertaxIndex){
     if(visit[vertaxIndex] == true) return;
     vector<int> &vertax = vertaxArray[vertaxIndex];
-    cout << vertaxIndex << "   ";
+    cout << vertaxIndex << " ";
     visit[vertaxIndex] = true;
     if(vertax.empty()) return;
     for(int i = 0; i != vertax.size(); i++){
         dfs(vertax[i]);
     }
-    cout << endl;
+}
+
+void bfs(int start){
+    queue<int> q;
+    q.push(start); // 처음 시작점을 큐에 추가
+    while(!q.empty()){
+        int front = q.front();
+        if(visit[front] == false){
+            cout << front << " ";
+            visit[front] = true;
+            for(int i = 0; i < vertaxArray[front].size(); i++){
+                q.push(vertaxArray[front][i]);
+            }
+        }
+        q.pop();
+    }
 }
 
 int main(){
@@ -28,10 +44,11 @@ int main(){
     for(int i = 1; vertaxArray[i].size() != 0; i++){
         sort(vertaxArray[i].begin(),vertaxArray[i].end());
     }
-    
-    for(int i = 0; i < 1001; i++) visit[i] = 0;
     dfs(startPoint);
-
+    for(int i = 0; i < 1001; i++) visit[i] = 0;
+    cout << endl;
+    bfs(startPoint);
+    cout << endl;
 }
 
 // void printVector(){
